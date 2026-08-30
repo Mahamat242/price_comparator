7111a1acf0a40690fb9a9dc380cf6a9572612d3d
Mahamat242 <harounm087@gmail.com>
2026-08-30 17:54:37 +0000

    mise en place des endpoints restants

--- backend/app/scrapers/base.py
+++ backend/app/scrapers/base.py
@@ -12,7 +12,12 @@ class Base:
     def __init__(self, baseUrl: str):
         self.baseUrl = baseUrl
         self.headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/151.0.0.0 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Referer": "https://www.google.com/",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1"
         }
 
     def clean_price(self, lettre: str) -> float:
